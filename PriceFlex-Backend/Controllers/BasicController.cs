4e48a6247969e487a3b6abbb74159d13465f2ac1
Piotrko64 <Piotrpc77@gmail.com>
2024-06-16 19:14:59 +0200

    Add project files.

---
+++ PriceFlex-Backend/Controllers/BasicController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PriceFlex_Backend.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PriceFlex_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasicController : ControllerBase
+    {
+        WebScrapperService _webscrapper;
+
+        public BasicController(WebScrapperService webScrapper)
+        {
+            _webscrapper = webScrapper;
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public string Get()
+        {
+            return _webscrapper.GetPriceByUrlAndClasses("https://www.mediaexpert.pl/komputery-i-tablety/laptopy-i-ultrabooki/laptopy/laptop-lenovo-ideapad-gaming-3-15ach6-15-6-ips-144hz-r5-5500h-16gb-ram-512gb-ssd-geforce-rtx2050", ".main-price .whole");
+        }
+
+    }
+}
---
+++ PriceFlex-Backend/Services/WebScrapperService.cs
@@ -0,0 +1,28 @@
+﻿using HtmlAgilityPack;
+using HtmlAgilityPack.CssSelectors.NetCore;
+
+namespace PriceFlex_Backend.Services
+{
+    public class WebScrapperService
+    {
+
+       
+
+        public string GetPriceByUrlAndClasses(string url, string classes)
+        {
+
+            
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+            var content = doc.QuerySelector(classes).InnerText;
+            
+
+            return content;
+
+        }
+
+
+
+
+    }
+}
